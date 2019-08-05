be5c98a0f210604b0d21c6b1c6d27afeb08c94d2
Lars <lars@roth-kl.de>
2019-08-05 09:15:10 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -2,13 +2,14 @@ import os
 from input_parser import load_sentences_from_file
 from analyzer import Analyzer
 
-FILE_TO_ANALYZE = os.getcwd() + "/samples/sample_sofea.txt"
+FILE_TO_ANALYZE = os.getcwd() + "/samples/debug.txt"
 WARNING_THRESHOLD = 20
-PREFIX = "Sofea_Hebbit"
+WORD_GAP = 5
+PREFIX = "Debug"
 
 
 if __name__ == '__main__':
     print(":: Text Analyzer")
     sentences = load_sentences_from_file(FILE_TO_ANALYZE)
-    analyzer = Analyzer(sentences, WARNING_THRESHOLD, PREFIX)
+    analyzer = Analyzer(sentences, WARNING_THRESHOLD, PREFIX, WORD_GAP)
     analyzer.run_analyses()
